94b736581f9a7c594db5a376e8a5ad3cb814bd6d
Nam <64239237+yudhishthiradattachaudhuri@users.noreply.github.com>
2020-08-11 23:06:16 +1000

    Add files via upload

---
+++ finalexam/trees/TreeIsPerfectlyBalanced.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+
+#include "tree.h"
+
+int nNodes(Tree t);
+
+bool TreeIsPerfectlyBalanced(Tree t) {
+    // if tree = NULL then it is balanced by default
+    // else, need to satisfy abs(nnodes(left)-nnodes(right)) <= 1
+    // and rest of tree is perfectly balanced
+    return (t == NULL || (TreeIsPerfectlyBalanced(t->left) && TreeIsPerfectlyBalanced(t->right) && 
+    abs(nNodes(t->left) - nNodes(t->right) <= 1)));
+}
+
+// find number of nodes in a (sub)tree
+int nNodes(Tree t) {
+    if (t == NULL) return 0;
+    return 1 + nNodes(t->left) + nNodes(t->right);
+}
