f94cb9f96af9448a9f0c48133fc16c6815604060
Nam <64239237+yudhishthiradattachaudhuri@users.noreply.github.com>
2020-08-12 11:45:09 +1000

    Update BSTreeGetSmallest.c

--- finalexam/trees/BSTreeGetSmallest.c
+++ finalexam/trees/BSTreeGetSmallest.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 BSTree BSTreeGetSmallest(BSTree t) {
-    // keep going left until cannot go further to get smallest
+   	// keep going left until cannot go further to get smallest
 	if (t == NULL) return NULL;
 	else if (t->left == NULL) return t;
 	return BSTreeGetSmallest(t->left);
