69e8a23325e4ef26a1ab05c9fe29dff3cf84f64d
Nam <64239237+yudhishthiradattachaudhuri@users.noreply.github.com>
2020-08-12 21:59:49 +1000

    Add files via upload

---
+++ finalexam/trees/TreeSumOdds.c
@@ -0,0 +1,11 @@
+
+#include <stdlib.h>
+
+#include "tree.h"
+
+int TreeSumOdds(Tree t) {
+	if (t == NULL) return 0;
+	else if (t->value % 2 == 1) return t->value + TreeSumOdds(t->left) + TreeSumOdds(t->right);
+	else return TreeSumOdds(t->left) + TreeSumOdds(t->right);
+}
+
